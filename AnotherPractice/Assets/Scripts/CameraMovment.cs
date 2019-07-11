a1a955e5b9d2e95d4f1dffaf57b259d1d330ebb2
empios <pawel.wlo@wp.pl>
2019-07-11 14:14:43 +0200

    Lerp practice

---
+++ AnotherPractice/Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+	public Transform[] cameraPosition;
+	private int count = 1;
+	private float speed = 2f;
+	
+
+	void Update()
+	{
+	if(Input.GetKeyDown(KeyCode.F))
+		{
+			if (count == 3)
+			{
+				count = 0;
+			}
+			else
+				count++;
+		}
+	}
+	
+	private void FixedUpdate()
+	{
+		transform.position = Vector3.Lerp(transform.position, cameraPosition[count].position, speed * Time.deltaTime);
+		transform.rotation = Quaternion.Lerp(transform.rotation, cameraPosition[count].rotation, speed * Time.deltaTime);
+	}
+}
